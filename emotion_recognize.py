c36631491dab9f2df6d9c7598a359214751301e2
coall132 <kaeligroche132@gmail.com>
2024-04-15 11:37:45 +0200

    a

---
+++ emotion_recognize.py
@@ -0,0 +1,161 @@
+import pandas as pd
+import numpy as np
+
+import os
+import sys
+
+# librosa is a Python library for analyzing audio and music. It can be used to extract the data from the audio files we will see it later.
+import librosa
+import librosa.display
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+
+# to play the audio files
+from IPython.display import Audio
+
+import keras
+from keras.callbacks import ReduceLROnPlateau
+from keras.models import Sequentia
+
+from keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Dropout, BatchNormalization
+from keras.utils import np_utils, to_categorical
+from keras.callbacks import ModelCheckpoint
+
+import warnings
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+Ravdess = "/kaggle/input/ravdess-emotional-speech-audio/audio_speech_actors_01-24/"
+Crema = "/kaggle/input/cremad/AudioWAV/"
+Tess = "/kaggle/input/toronto-emotional-speech-set-tess/tess toronto emotional speech set data/TESS Toronto emotional speech set data/"
+Savee = "/kaggle/input/surrey-audiovisual-expressed-emotion-savee/ALL/"
+
+ravdess_directory_list = os.listdir(Ravdess)
+
+file_emotion = []
+file_path = []
+for dir in ravdess_directory_list:
+    # as their are 20 different actors in our previous directory we need to extract files for each actor.
+    actor = os.listdir(Ravdess + dir)
+    for file in actor:
+        part = file.split('.')[0]
+        part = part.split('-')
+        # third part in each file represents the emotion associated to that file.
+        file_emotion.append(int(part[2]))
+        file_path.append(Ravdess + dir + '/' + file)
+        
+# dataframe for emotion of files
+emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
+
+# dataframe for path of files.
+path_df = pd.DataFrame(file_path, columns=['Path'])
+Ravdess_df = pd.concat([emotion_df, path_df], axis=1)
+
+# changing integers to actual emotions.
+Ravdess_df.Emotions.replace({1:'neutral', 2:'calm', 3:'happy', 4:'sad', 5:'angry', 6:'fear', 7:'disgust', 8:'surprise'}, inplace=True)
+Ravdess_df.head()
+
+crema_directory_list = os.listdir(Crema)
+
+file_emotion = []
+file_path = []
+
+for file in crema_directory_list:
+    # storing file paths
+    file_path.append(Crema + file)
+    # storing file emotions
+    part=file.split('_')
+    if part[2] == 'SAD':
+        file_emotion.append('sad')
+    elif part[2] == 'ANG':
+        file_emotion.append('angry')
+    elif part[2] == 'DIS':
+        file_emotion.append('disgust')
+    elif part[2] == 'FEA':
+        file_emotion.append('fear')
+    elif part[2] == 'HAP':
+        file_emotion.append('happy')
+    elif part[2] == 'NEU':
+        file_emotion.append('neutral')
+    else:
+        file_emotion.append('Unknown')
+        
+# dataframe for emotion of files
+emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
+
+# dataframe for path of files.
+path_df = pd.DataFrame(file_path, columns=['Path'])
+Crema_df = pd.concat([emotion_df, path_df], axis=1)
+Crema_df.head()
+
+tess_directory_list = os.listdir(Tess)
+
+file_emotion = []
+file_path = []
+
+for dir in tess_directory_list:
+    directories = os.listdir(Tess + dir)
+    for file in directories:
+        part = file.split('.')[0]
+        part = part.split('_')[2]
+        if part=='ps':
+            file_emotion.append('surprise')
+        else:
+            file_emotion.append(part)
+        file_path.append(Tess + dir + '/' + file)
+        
+# dataframe for emotion of files
+emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
+
+# dataframe for path of files.
+path_df = pd.DataFrame(file_path, columns=['Path'])
+Tess_df = pd.concat([emotion_df, path_df], axis=1)
+Tess_df.head()
+
+savee_directory_list = os.listdir(Savee)
+
+file_emotion = []
+file_path = []
+
+for file in savee_directory_list:
+    file_path.append(Savee + file)
+    part = file.split('_')[1]
+    ele = part[:-6]
+    if ele=='a':
+        file_emotion.append('angry')
+    elif ele=='d':
+        file_emotion.append('disgust')
+    elif ele=='f':
+        file_emotion.append('fear')
+    elif ele=='h':
+        file_emotion.append('happy')
+    elif ele=='n':
+        file_emotion.append('neutral')
+    elif ele=='sa':
+        file_emotion.append('sad')
+    else:
+        file_emotion.append('surprise')
+        
+# dataframe for emotion of files
+emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
+
+# dataframe for path of files.
+path_df = pd.DataFrame(file_path, columns=['Path'])
+Savee_df = pd.concat([emotion_df, path_df], axis=1)
+Savee_df.head()
+
+data_path = pd.concat([Ravdess_df, Crema_df, Tess_df, Savee_df], axis = 0)
+data_path.to_csv("data_path.csv",index=False)
+data_path.head()
+
+plt.title('Count of Emotions', size=16)
+sns.countplot(data_path.Emotions)
+plt.ylabel('Count', size=12)
+plt.xlabel('Emotions', size=12)
+sns.despine(top=True, right=True, left=False, bottom=False)
+plt.show()
