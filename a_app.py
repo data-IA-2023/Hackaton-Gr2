bf10d50c840edf83687939d76d4199737935c918
Laamh <clement.nogues.pro@gmail.com>
2024-04-18 14:26:53 +0200

    "maj streamlit"

--- a_app.py
+++ a_app.py
@@ -4,32 +4,18 @@ from d_record import audio
 import os
 import keras
 import streamlit as st
+import hydralit_components as hc
 
 st.set_page_config(page_title="EmoBot 🤖")
 
-st.markdown("<h1 style='text-align: center;'><u>Reconnaissance vocale d'émotions</u></h1>", unsafe_allow_html=True)
-
+st.markdown("<h1 style='text-align: center;'>Discute avec Emobot !</h1>", unsafe_allow_html=True)
+st.markdown("---")
 #===== Enregistrement de l'audio =====
 
 try:
     if st.button(label="Appuyez-ici pour débuter l'enregistrement"):
-        audio()
-        
+        with hc.HyLoader('',hc.Loaders.standard_loaders,index=5):
+            audio()
+            
 except:
-    st.write("Erreur de l'enregistrement")
-
-
-try:
-    path = "output.wav"
-    if os.path.exists(path):
-        try:
-            if st.button("Discuter avec EmoBot"):
-                predicted_emotion = predict_emotion(path)
-                print(predicted_emotion)
-                st.write(predicted_emotion)
-
-        except Exception as e :
-            st.write(f"Erreur du modele,{e}")
-
-except:
-    st.write("Erreur lors du chargement de l'audio")
+    print("sah c'est domaj")
